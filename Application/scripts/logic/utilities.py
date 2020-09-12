1844969ce2dcbe83f95c9f498df951c0c106f3b4
Ovsienko023 <Ovsienko023@gmail.com>
2020-09-12 11:49:55 +0300

    true version unique user

--- Application/scripts/logic/utilities.py
+++ Application/scripts/logic/utilities.py
@@ -4,6 +4,10 @@ import random
 import string
 
 
+class WriteError(Exception):
+    """Database write error."""
+
+
 class User:
     def __init__(self, u_id):
         self.u_id = u_id
@@ -12,9 +16,15 @@ class User:
 
     @classmethod
     def create(cls, name, password):
-        print('Создание пользователя')
-        pass
-
+        try:
+            print('Создание пользователя')
+            status = WrapperDB().create_user(name, password)
+            if status:
+                return {"Status": True, "info": "User added successfully."}
+            return {"Status": False, "info": "A user with the same name already exists."}
+        except WriteError:
+            return {"Status": False, "info": "user not added"}
+        
 
 
 class WrapperDB:
@@ -27,14 +37,22 @@ class WrapperDB:
             return data
     
     def commit(self):
-        with open(sys.path[0] + "/DataBase.json", "w") as write_file:
-            json.dump(self.data, write_file)
+        """Saving data to the database."""
+        try:
+            with open(sys.path[0] + "/DataBase.json", "w") as write_file:
+                json.dump(self.data, write_file)
+            return True
+        except:
+            raise WriteError
 
     def is_user_name(self, user_name):
-        for info in self.data.values():
-            if info['name'] == user_name:
-                return True
-        return False
+        try:
+            for info in list(self.data['users'].values()):
+                if info['name'] == user_name:
+                    return True
+            return False
+        except KeyError:
+            return False
 
     def get_new_id(self):
         """Generates a unique id for a new user."""
@@ -54,3 +72,24 @@ class WrapperDB:
     def get_password(self, u_id):
         """Returns the user's password by its id."""
         return self.data['users'][u_id]['password']
+    
+    def create_user(self, name, password):
+        if not WrapperDB().is_user_name(name):
+            user_id = self.get_new_id()
+            self.data['users'][user_id] = {"name": name, "password": password}
+            self.commit()
+            return True
+        return False
+
+    def get_all_users(self):
+        try:
+            data = {"users":[], "status": False}
+            user_list = list()
+            for user_id in self.data['users']:
+                self.data['users'][user_id]['id'] = user_id
+                data['users'].append(self.data['users'][user_id])
+            data['status'] = True
+            return data
+        except KeyError:
+            return data
+        
--- Application/scripts/server/flask_server.py
+++ Application/scripts/server/flask_server.py
@@ -1,17 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 from scripts.logic.utilities import WrapperDB
 from scripts.logic.utilities import User
 app = Flask(__name__)
 
 
+@app.route('/api/v1/adding_new_user', methods=['POST'])
+def adding_new_user():
+    """
+    {"name": "Bob", password: "123"}
+    """
+    data = request.json
+    name = data['name']
+    password = data['password']
+    status = User.create(name, password)
+    return status
+
+
 @app.route('/api/v1/user/list', methods=['GET'])
 def user_list():
-    """ GET all user """
-    a = WrapperDB()
-    # a.data['users']['2'] = {"name": "Kop", "password": "1213"}
-    # print(a.data)
-    # print(a.data['users']['1'])
-    # a.commit()
-    # print(a.get_new_id())
-    
-    return ""
+    """Get all user."""
+    user_list = WrapperDB().get_all_users()
+    return user_list
+
+@app.route('/api/v1/update/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    return "{}"
